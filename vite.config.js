bc58b3fbc910c86d9fb311a998fbc3c6ff916a4e
Bianca-Caetano <biancacaetano079@gmail.com>
2025-12-05 07:15:11 -0300

    adicionando vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import viteImagemin from 'vite-plugin-imagemin'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
-      },
-      optipng: {
-        optimizationLevel: 7,
-      },
-      mozjpeg: {
-        quality: 20,
-      },
-      pngquant: {
-        quality: [0.8, 0.9],
-        speed: 4,
-      },
-      svgo: {
-        plugins: [
-          {
-            name: 'removeViewBox',
-          },
-          {
-            name: 'removeEmptyAttrs',
-            active: false,
-          },
-        ],
-      },
-    }),
-  ],
-  server: {
-    host: true
-  }
+  plugins: [vue()],
+  base: './'
 })
